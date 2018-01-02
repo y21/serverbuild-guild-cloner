0619377fd81877150aa026b676f8a919723a3efa
Timo <30553356+y21@users.noreply.github.com>
2018-01-02 20:52:49 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+const fs = require("fs");
+var template = {};
+
+client.on("message", message => {
+    if(message.author.id === client.user.id || message.author.id === config.ownerid){
+        if(message.content.startsWith(".guild-clone") && message.guild){
+            var template = {
+                textChannels: [],
+                textChannelTopic: [],
+                voiceChannels: [],
+                roles: [],
+                roleColors: [],
+                emojiURLs: [],
+                emojiNames: [],
+                verificationLevel:0,
+                servername:0,
+                serverIcon:0
+            };
+              template.textChannels = message.guild.channels.filterArray(c => c.type === "text").map(c => c.name);
+              template.textChannelTopic = message.guild.channels.filter(c => c.type === "text").map(c => c.topic);
+              template.voiceChannels = message.guild.channels.filterArray(c => c.type === "voice");
+              template.roles = message.guild.roles.map(r => r.name);
+              template.roleColors = message.guild.roles.map(r => r.hexColor);
+              template.emojiURLs = message.guild.emojis.map(e => e.url);
+              template.emojiNames = message.guild.emojis.map(e => e.name);
+              template.verificationLevel = message.guild.verificationLevel;
+              template.servername = message.guild.name;
+              template.serverIcon = message.guild.iconURL;
+              console.log("Done!");
+              fs.writeFileSync("./result.json", JSON.stringify(template));
+        }
+    }
+});
+
+
+client.login(config.token);
